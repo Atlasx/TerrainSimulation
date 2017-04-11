89e628737229889ec10e1d18cd21d0cab5dda1f5
Connor Douglas <cjdouglas96@gmail.com>
2017-04-11 12:04:52 -0600

    Initial Commit

---
+++ TerrainProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TerrainProject
+//
+//  Created by Connor Douglas on 4/11/17.
+//  Copyright © 2017 Connor Douglas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
